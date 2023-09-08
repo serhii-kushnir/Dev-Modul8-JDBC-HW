e404b191109f0112f90604a600e406aa2ab38650
fear1611 <127629681+fear1611@users.noreply.github.com>
2023-09-08 17:09:32 +0300

    added log4j

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import org.apache.log4j.Logger;
 
+public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("\nStarted...");
 
     }
 }
